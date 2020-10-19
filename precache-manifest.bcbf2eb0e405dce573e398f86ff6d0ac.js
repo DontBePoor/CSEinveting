933243719121f31a8db8d296f4cfd413f1b4be07
JanithaGit <janithaprashadkatukenda@gmail.com>
2020-10-19 12:36:10 +0530

    Updates

---
+++ precache-manifest.bcbf2eb0e405dce573e398f86ff6d0ac.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99d64e0161854fbf93d91e0594691c6",
+    "url": "/CSEinvesting/index.html"
+  },
+  {
+    "revision": "76a38886c8ee8bc536a2",
+    "url": "/CSEinvesting/static/css/2.7ce04e15.chunk.css"
+  },
+  {
+    "revision": "2d42397b8979380f1816",
+    "url": "/CSEinvesting/static/css/3.06890bda.chunk.css"
+  },
+  {
+    "revision": "931c08de50d3902afa28",
+    "url": "/CSEinvesting/static/css/main.08333ebc.chunk.css"
+  },
+  {
+    "revision": "76a38886c8ee8bc536a2",
+    "url": "/CSEinvesting/static/js/2.6880e628.chunk.js"
+  },
+  {
+    "revision": "d69b8322de154fd9177d66d671415220",
+    "url": "/CSEinvesting/static/js/2.6880e628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d42397b8979380f1816",
+    "url": "/CSEinvesting/static/js/3.8aac034e.chunk.js"
+  },
+  {
+    "revision": "7c05b89a15a8a5dc2ea3",
+    "url": "/CSEinvesting/static/js/4.3a0d1534.chunk.js"
+  },
+  {
+    "revision": "e73c07d2d0b6f07699fe",
+    "url": "/CSEinvesting/static/js/5.1185041b.chunk.js"
+  },
+  {
+    "revision": "b79b8fd4cebba5a925ca",
+    "url": "/CSEinvesting/static/js/6.4e971471.chunk.js"
+  },
+  {
+    "revision": "004445829e3c74729505",
+    "url": "/CSEinvesting/static/js/7.b14beadf.chunk.js"
+  },
+  {
+    "revision": "931c08de50d3902afa28",
+    "url": "/CSEinvesting/static/js/main.422f8343.chunk.js"
+  },
+  {
+    "revision": "af5d9594c71f1f32b173",
+    "url": "/CSEinvesting/static/js/runtime-main.fb706497.js"
+  },
+  {
+    "revision": "479ba6af27cb24246e1dfdace6c53e63",
+    "url": "/CSEinvesting/static/media/DunningCrug.479ba6af.jpg"
+  },
+  {
+    "revision": "17cb9303df7b7264b9c4ef0953366617",
+    "url": "/CSEinvesting/static/media/Gilroy-Bold.17cb9303.otf"
+  },
+  {
+    "revision": "d90fa984926d3745c7e68bc525692c26",
+    "url": "/CSEinvesting/static/media/Gilroy-Light.d90fa984.otf"
+  },
+  {
+    "revision": "9e12d6053ae1c539d9be9d6fa86d33a7",
+    "url": "/CSEinvesting/static/media/Gilroy-Medium.9e12d605.otf"
+  },
+  {
+    "revision": "56a65ab2f80da670adcd42a483f92123",
+    "url": "/CSEinvesting/static/media/bg-img.56a65ab2.png"
+  },
+  {
+    "revision": "b1ff251ba2e50cbcf88c58278b6aa366",
+    "url": "/CSEinvesting/static/media/laffer.b1ff251b.jpg"
+  },
+  {
+    "revision": "56a65ab2f80da670adcd42a483f92123",
+    "url": "/CSEinvesting/static/media/logo.56a65ab2.png"
+  },
+  {
+    "revision": "a28f35c596beddb49e1d99f0e0b429a0",
+    "url": "/CSEinvesting/static/media/logo2.a28f35c5.jpg"
+  }
+]);
---
+++ static/js/6.4e971471.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcseinvesting=this.webpackJsonpcseinvesting||[]).push([[6],{995:function(e,t,n){"use strict";n.r(t);var a=n(26),r=n(27),c=n(29),s=n(28),i=n(1),u=n.n(i),o=function(e){Object(c.a)(n,e);var t=Object(s.a)(n);function n(){var e;Object(a.a)(this,n);for(var r=arguments.length,c=new Array(r),s=0;s<r;s++)c[s]=arguments[s];return(e=t.call.apply(t,[this].concat(c))).state={},e}return Object(r.a)(n,[{key:"render",value:function(){return u.a.createElement("h1",null,"Save as Draft")}}]),n}(i.Component);t.default=o}}]);
+//# sourceMappingURL=6.4e971471.chunk.js.map
